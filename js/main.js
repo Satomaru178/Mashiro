647877c974c10165232de46cd856ed9cc892b386
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-21 12:01:54 +0900

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,58 @@
+'use strict';
+{
+  $(function () {
+    /*=================================================
+    ハンバーガーメニュー
+    ===================================================*/
+    $('.menu-btn, #mask').on('click', function () {
+      $('.menu-btn, #nav, #mask').toggleClass('open');
+    });
+
+    /*=================================================
+    スムーススクロール
+    ===================================================*/
+    $('a[href^="#"]').click(function(){
+      let href= $(this).attr("href");
+      let target = $(href == "#" || href == "" ? 'html' : href);
+      let position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, 600, "swing");
+      return false;
+    });
+
+    /*=================================================
+    スライダー
+    ===================================================*/
+    // $('.slick-slider').slick({
+    //   arrows: false,
+    //   centerMode: true,
+    //   centerPadding: '100px',
+    //   slidesToShow: 3,
+    //   autoplay: true,
+    //   autoplaySpeed: 2000,
+    //   responsive: [
+    //     {
+    //       breakpoint: 768,
+    //       settings: {
+    //         centerPadding: '50px',
+    //         slidesToShow: 1
+    //       }
+    //     }
+    //   ]
+    // });
+
+    /*=================================================
+    フェードイン
+    ===================================================*/
+    // $(window).scroll(function (){
+    //   $('.fadein').each(function(){
+    //     var targetElement = $(this).offset().top;
+    //     var scroll = $(window).scrollTop();
+    //     var windowHeight = $(window).height();
+    //     if (scroll > targetElement - windowHeight){
+    //       $(this).css('opacity','1');
+    //       $(this).css('transform','translateY(0)');
+    //     }
+    //   });
+    // });
+  });
+}
