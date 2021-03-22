1529bdb1064874a98b9431270568090ba871b338
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-22 12:17:34 +0900

    scrolled fix menu

--- js/main.js
+++ js/main.js
@@ -20,6 +20,23 @@
     });
 
     /*=================================================
+    スクロール後のヘッダーメニュー固定
+    ===================================================*/
+    var $win = $(window),
+    $header = $('#header'),
+    headerPos = $header.offset().top,
+    fixedClass = 'fix';
+
+    $win.on('load scroll', function () {
+      var value = $(this).scrollTop();
+      if (value > headerPos) {
+        $header.addClass(fixedClass);
+      } else {
+        $header.removeClass(fixedClass);
+      }
+    });
+
+    /*=================================================
     スライダー
     ===================================================*/
     // $('.slick-slider').slick({
